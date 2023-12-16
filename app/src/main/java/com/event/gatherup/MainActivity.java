28f1a5e48cde5f80fba061ce9132990447cac037
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-12-17 01:37:15 +0530

    OnBoarding Skeleton done

--- app/src/main/java/com/event/gatherup/MainActivity.java
+++ app/src/main/java/com/event/gatherup/MainActivity.java
@@ -1,14 +1,35 @@
 package com.event.gatherup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView get_started_cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // for full screen
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        get_started_cv = findViewById(R.id.getStarted);
+
+        get_started_cv.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, OnBoarding.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
